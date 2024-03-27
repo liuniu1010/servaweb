a7db1c641d5e0a978065556ca5544d7fe842003c
Niu Liu <liuniu@tsinghua.org.cn>
2024-03-27 16:48:50 +1300

    add first function call method

---
+++ src/main/java/org/neo/servaweb/impl/FunctionCallImpl.java
@@ -0,0 +1,61 @@
+package org.neo.servaweb.ifc;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.neo.servaweb.model.AIModel;
+import org.neo.servaweb.ifc.FunctionCallIFC;
+import org.neo.servaweb.util.CommonUtil;
+
+public class FunctionCallImpl implements FunctionCallIFC {
+    public List<AIModel.Function> getFunctions() {
+        // executeCommand
+        AIModel.Function executeCommand = generateFunctionForExecuteCommand();
+
+        List<AIModel.Function> functions = new ArrayList<AIModel.Function>();
+        functions.add(executeCommand);
+        return functions;
+    }
+
+    public Object callFunction(AIModel.Call call) {
+        if(call.getMethodName().equals(METHODNAME_EXECUTECOMMAND)) {
+            return call_executeCommand(call);
+        }
+
+        return null;
+    }
+
+    private AIModel.Function generateFunctionForExecuteCommand() {
+        AIModel.FunctionParam param = new AIModel.FunctionParam();
+        param.setName(EXECUTECOMMAND_PARAM_COMMAND);
+        param.setDescription("the command to be executed");
+
+        List<AIModel.FunctionParam> params = new ArrayList<AIModel.FunctionParam>();
+        params.add(param);
+
+        AIModel.Function function = new AIModel.Function();
+        function.setMethodName(METHODNAME_EXECUTECOMMAND);
+        function.setParams(params);
+        function.setDescription("to execute command on local machine");
+
+        return function;
+    }
+
+    private static String METHODNAME_EXECUTECOMMAND = "executeCommand";
+    private static String EXECUTECOMMAND_PARAM_COMMAND = "command";
+    private String executeCommand(String command) {
+        return CommonUtil.executeCommand(command);
+    }
+
+    private String call_executeCommand(AIModel.Call call) {
+        List<AIModel.CallParam> params = call.getParams();
+        String command = null;
+        for(AIModel.CallParam param: params) {
+            if(param.getName().equals(EXECUTECOMMAND_PARAM_COMMAND)) {
+                command = param.getValue();
+            }
+        }
+
+        return executeCommand(command);
+    }
+}
