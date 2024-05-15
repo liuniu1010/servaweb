66f4def217672e213cd6f49f625baf9dbc665194
Niu Liu <liuniu@tsinghua.org.cn>
2024-05-16 10:00:02 +1200

    add chatwithspeechtotextexpert.html

---
+++ src/main/java/org/neo/servaweb/webservice/AIChatWithSpeechToTextExpert.java
@@ -0,0 +1,63 @@
+package org.neo.servaweb.webservice;
+
+import java.io.File;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.neo.servaaiagent.ifc.ChatForUIIFC;
+import org.neo.servaaiagent.impl.ChatWithSpeechToTextExpertForUIImpl;
+
+@Path("/aichatwithspeechtotextexpert")
+public class AIChatWithSpeechToTextExpert extends AbsAIChat {
+    private String audiosFolder = "audios";
+
+    protected String getOnlineFileAbsolutePath() {
+        return super.getAbsoluteResourcePath() + File.separator + audiosFolder;
+    }
+
+    protected String getRelavantVisitPath() {
+        return audiosFolder;
+    }
+
+    @Override
+    protected ChatForUIIFC getChatForUIInstance() {
+        return ChatWithSpeechToTextExpertForUIImpl.getInstance(getOnlineFileAbsolutePath(), getRelavantVisitPath());
+    }
+
+    @POST
+    @Path("/send")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse send(WSModel.AIChatParams params) {
+        return super.send(params);
+    }
+
+    @POST
+    @Path("/echo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse echo(WSModel.AIChatParams params) {
+        return super.echo(params);
+    }
+
+    @POST
+    @Path("/newchat")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse newchat(WSModel.AIChatParams params) {
+        return super.newchat(params);
+    }
+
+    @POST
+    @Path("/refresh")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse refresh(WSModel.AIChatParams params) {
+        return super.refresh(params);
+    }
+}
