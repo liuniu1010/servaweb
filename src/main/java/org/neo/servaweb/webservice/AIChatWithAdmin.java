a85ade1e6362f8e51fbac98022c8bcd7a7439082
Niu Liu <liuniu@tsinghua.org.cn>
2024-07-07 13:03:37 +1200

    add chat with admin

---
+++ src/main/java/org/neo/servaweb/webservice/AIChatWithAdmin.java
@@ -0,0 +1,51 @@
+package org.neo.servaweb.webservice;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.neo.servaaiagent.ifc.ChatForUIIFC;
+import org.neo.servaaiagent.impl.ChatWithAdminForUIImpl;
+
+@Path("/aichatwithadmin")
+public class AIChatWithAdmin extends AbsAIChat {
+    @Override
+    protected ChatForUIIFC getChatForUIInstance() {
+        return ChatWithAdminForUIImpl.getInstance();
+    }
+
+    @POST
+    @Path("/send")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse send(WSModel.AIChatParams params) {
+        return super.send(params);
+    }
+
+    @POST
+    @Path("/echo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse echo(WSModel.AIChatParams params) {
+        return super.echo(params);
+    }
+
+    @POST
+    @Path("/newchat")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse newchat(WSModel.AIChatParams params) {
+        return super.newchat(params);
+    }
+
+    @POST
+    @Path("/refresh")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WSModel.AIChatResponse refresh(WSModel.AIChatParams params) {
+        return super.refresh(params);
+    }
+}
