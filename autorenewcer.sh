df3e23021f4a382503b22058af7e4ab4fb909e93
Niu Liu <liuniu@tsinghua.org.cn>
2024-07-14 17:04:27 +1200

    simplify autorenewcer.sh

--- autorenewcer.sh
+++ autorenewcer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run acme.sh renewal command
-~/.acme.sh/acme.sh --cron --home ~/.acme.sh/
+$HOME/.acme.sh/acme.sh --cron --home $HOME/.acme.sh/
 
 # Get certificate path
-CERT_DIR=~/.acme.sh/neoai.bot_ecc
+CERT_DIR=$HOME/.acme.sh/neoai.bot_ecc
 FULLCHAIN=$CERT_DIR/fullchain.cer
 
 # Check if renewal was successful or needed
@@ -18,28 +18,13 @@ if [ $(find $FULLCHAIN -mtime -1) ]; then
   echo "Certificate has been renewed, proceeding with the following steps"
 
   # Define other paths
-  GIT_DIR=~/git/github/servaweb
   CONTAINER_NAME=servaweb
   TOMCAT_CONF_DIR=/usr/local/tomcat/conf
 
-  # Copy new certificate and key files to local GitHub repository path
-  cp $CERT_DIR/fullchain.cer $GIT_DIR/
-  cp $CERT_DIR/neoai.bot.key $GIT_DIR/
-  cp $CERT_DIR/ca.cer $GIT_DIR/
-
-  # Check if copying was successful
-  if [ $? -ne 0 ]; then
-    echo "Failed to copy certificate files"
-    exit 1
-  fi
-
-  # Change to the GitHub repository directory
-  cd $GIT_DIR
-
   # Copy certificate files to Docker container
-  docker cp fullchain.cer $CONTAINER_NAME:$TOMCAT_CONF_DIR/
-  docker cp neoai.bot.key $CONTAINER_NAME:$TOMCAT_CONF_DIR/
-  docker cp ca.cer $CONTAINER_NAME:$TOMCAT_CONF_DIR/
+  docker cp $CERT_DIR/fullchain.cer $CONTAINER_NAME:$TOMCAT_CONF_DIR/
+  docker cp $CERT_DIR/neoai.bot.key $CONTAINER_NAME:$TOMCAT_CONF_DIR/
+  docker cp $CERT_DIR/ca.cer $CONTAINER_NAME:$TOMCAT_CONF_DIR/
 
   # Check if copying to Docker container was successful
   if [ $? -ne 0 ]; then
