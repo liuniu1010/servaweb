981691460f66f886de7686a23cceeff6928e53da
Niu Liu <liuniu@tsinghua.org.cn>
2024-09-12 16:27:46 +1200

    some adjustments on local setup scripts

--- runimage_local.sh
+++ runimage_local.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 --add-host=mydb:192.168.122.28 --name servaweb_local servaweb_local:0.1
+docker run -d -p 8080:8080 --add-host=mydb:<dbip> --name servaweb_local servaweb_local:0.1
