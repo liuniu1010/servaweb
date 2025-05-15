9c4fba5594a4ce4f2dccee561f6942d648fdc582
Niu Liu <liuniu@tsinghua.org.cn>
2025-05-15 15:36:44 +1200

    adjust nginx name

--- runnginx.sh
+++ runnginx.sh
@@ -1,6 +1,6 @@
 docker run -d \
 -p 443:443 \
---name nginx_ssl \
+--name nginx_web \
 --restart=unless-stopped \
 -v <conffileonhost>:/etc/nginx/nginx.conf:ro \
 -v <sslpathonhost>:/etc/nginx/ssl:ro \
