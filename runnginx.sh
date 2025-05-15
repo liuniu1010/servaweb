cce675ae55ea2f5df6897cb3fd284332ee72ffa5
Niu Liu <liuniu@tsinghua.org.cn>
2025-05-15 14:49:37 +1200

    map path readonly for nginx

--- runnginx.sh
+++ runnginx.sh
@@ -2,6 +2,6 @@ docker run -d \
 -p 443:443 \
 --name nginx_ssl \
 --restart=unless-stopped \
--v <conffileonhost>:/etc/nginx/nginx.conf \
--v <sslpathonhost>:/etc/nginx/ssl \
+-v <conffileonhost>:/etc/nginx/nginx.conf:ro \
+-v <sslpathonhost>:/etc/nginx/ssl:ro \
 nginx_ssl:0.1
